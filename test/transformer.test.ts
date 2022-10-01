ba6bb3ead649b16c595c569926e01c82869b6704
Olivier Huin <olih@users.noreply.github.com>
2022-10-01 13:30:58 +0100

    Fixes issue with flags and add nodejs test (#3)

--- test/transformer.test.ts
+++ test/transformer.test.ts
@@ -1,10 +1,34 @@
+import { it } from 'node:test';
+import { deepStrictEqual } from 'node:assert';
 import { parseOutputScript } from '../src/transformer.js';
 
-describe('transformer', () => {
-  it('should parse output script', () => {
-    const actual = parseOutputScript(
-      'if status equals success then abstract else pass'
-    );
-    expect(actual).toMatchInlineSnapshot();
-  });
+it('should parse output script', () => {
+  const actual = parseOutputScript(
+    'if status equals success then abstract else pass'
+  );
+  const expected = {
+    status: 'success',
+    value: {
+      key: 'status',
+      value: 'success',
+      onSuccess: ['abstract'],
+      onFailure: ['pass'],
+    },
+  };
+  deepStrictEqual(actual, expected);
+});
+it('should parse output script with multiple flags', () => {
+  const actual = parseOutputScript(
+    'if status equals success then abstract else count sha256'
+  );
+  const expected = {
+    status: 'success',
+    value: {
+      key: 'status',
+      value: 'success',
+      onSuccess: ['abstract'],
+      onFailure: ['count', 'sha256'],
+    },
+  };
+  deepStrictEqual(actual, expected);
 });
