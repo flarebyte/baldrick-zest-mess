aaefb0733a3eb0cda81b9fbe3cda0db36d05f81b
Olivier Huin <olih@users.noreply.github.com>
2022-10-07 13:47:51 +0100

    Adds type to toConfigList (#7)

--- test/runner.test.ts
+++ test/runner.test.ts
@@ -2,16 +2,18 @@ import { it } from 'node:test';
 import { deepStrictEqual } from 'node:assert';
 import { toConfigList } from '../src/runner.js';
 
-const toConfigFunction = (specFile: string) => ({ specFile });
+const toConfigFunction = (specFile: string) => ({ specFile , additional: 'info'});
 
 it('should search for spec files', async () => {
   const actual = await toConfigList('spec', toConfigFunction);
   deepStrictEqual(actual, [
     {
       specFile: 'spec/dummy.zest.yaml',
+      additional: 'info',
     },
     {
       specFile: 'spec/level2/dummy2.zest.yaml',
+      additional: 'info',
     },
   ]);
 });
